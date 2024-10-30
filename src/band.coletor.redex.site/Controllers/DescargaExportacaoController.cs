b242a710323939f4a965a3cd2d47bbc96a306052
kleiton freitas <kleiton@microled.com.br>
2024-10-30 17:41:34 -0300

    Inclui tela de Descarga Exportacao

---
+++ src/band.coletor.redex.site/Controllers/DescargaExportacaoController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace Band.Coletor.Redex.Site.Controllers
+{
+    public class DescargaExportacaoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
